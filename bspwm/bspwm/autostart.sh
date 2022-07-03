74e6c2c409f1ceecc74d91ec2a7c2ae2b860080c
nassimna <najjarnassim1995@gmail.com>
2022-07-03 16:39:27 +0100

    h

---
+++ bspwm/bspwm/autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "fr" ]; then
+  run sxhkd -c ~/.config//sxhkd/sxhkdrc &
+else
+  run sxhkd -c ~/.config//sxhkd/sxhkdrc-qwerty &
+fi
