197e39847fcac9ee0f6388b220a6db90f27ec0bf
nassimna <najjarnassim1995@gmail.com>
2022-07-06 16:05:54 +0100

    ADD FONTS

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 
-#bsp layout
-curl https://raw.githubusercontent.com/phenax/bsp-layout/master/install.sh | bash -;
+
 
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
@@ -8,17 +8,18 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Updating System
-dnf update -y
+pacman -Syu
 #PROGRAMS
 # polybar bspwm rofi dmenu fish mumble murmur zsh dunst rxvt-unicode picom sxhkd nautilus unzip
-pacman -S polybar bspwm rofi dmenu fish mumble murmur zsh dunst rxvt-unicode picom sxhkd nautilus unzip
+pacman -S polybar bspwm rofi dmenu fish mumble murmur zsh dunst rxvt-unicode picom sxhkd nautilus unzip lxappearance
 # MOVE EVERY THING TO HOME FOLDER ~
 
 #bsp layout
 curl https://raw.githubusercontent.com/phenax/bsp-layout/master/install.sh | bash -;
 #mumble 
-sudo systemctl stop mumble-server.service
-sudo systemctl disable mumble-server.service 
+systemctl stop mumble-server.service
+systemctl disable mumble-server.service 
+#themes here https://wiki.archlinux.org/title/GTK
 
 # Installing fonts
 
@@ -30,7 +31,10 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Liberation
 unzip LiberationMono.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
 unzip Agave.zip -d /usr/share/fonts
-unzip Agave.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+unzip RobotoMono.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Noto.zip
+unzip Noto.zip -d /usr/share/fonts
 unzip fontawesome-free-5.15.4-desktop.zip -d /usr/share/fonts
 unzip fontawesome-free-5.15.4-web.zip -d /usr/share/fonts
 # Reloading Font
